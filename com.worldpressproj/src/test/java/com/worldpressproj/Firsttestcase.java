ae3d945fe200c746b9e9575e3a636f829ba66ad6
Jagan <Jagan@Jagan-THINK>
2015-09-28 01:29:13 -0700

    Changed the output logging statement....

--- com.worldpressproj/src/test/java/com/worldpressproj/Firsttestcase.java
+++ com.worldpressproj/src/test/java/com/worldpressproj/Firsttestcase.java
@@ -2,6 +2,6 @@ package com.worldpressproj;
 
 public class Firsttestcase {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world updated");
 	}
 }
